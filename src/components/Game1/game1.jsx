1a1b93ec10ec91cb6d5ce30132bca9cb7e2adee1
DCarr40 <davidwcarruthers@yahoo.com>
2021-08-03 14:35:48 -0400

    set up boiler plate

---
+++ src/components/Game1/game1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import "./game1.css"
+
+const Game1 = props => {
+    return (
+        <div>
+            <div>Fish Game</div>
+            <canvas>Fish</canvas>
+        </div>
+    )
+}
+ Game1.propTypes = {
+
+}
+
+export default Game1
---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Game1 from './Game1/game1'
+
+const App = () => {
+    return (
+        <div>
+            <Game1/>
+        </div>
+    )
+}
+
+export default App;
