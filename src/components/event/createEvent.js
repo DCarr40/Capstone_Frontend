96c1d5c854b002f37ebd4675bbeee3dc13fdd483
DCarr40 <davidwcarruthers@yahoo.com>
2021-08-07 10:11:11 -0400

    added form to create events

--- src/components/event/createEvent.js
+++ src/components/event/createEvent.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+
+//form to create events
 const CreateEvent = () => {
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
