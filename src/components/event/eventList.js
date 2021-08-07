45b7af93b5cd74a6c7a4d417f535bc39d2fa50a5
DCarr40 <davidwcarruthers@yahoo.com>
2021-08-07 04:01:39 -0400

    was able to get props and display event titles

--- src/components/event/eventList.js
+++ src/components/event/eventList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import EventSummary from "./eventSummary";
 
-const EventList = () => {
+const EventList = ({events}) => {
   return (
     <div className="event-list section">
-      <EventSummary />
-      <EventSummary />
-      <EventSummary />
-      <EventSummary />
+      {/*&& means "then" in this case. If events does exist then map*/}
+        {events && events.map((event) =>{
+          return (<EventSummary event={event} key={event.id}/>)
+        })}
     </div>
   );
 };
--- src/components/event/eventSummary.js
+++ src/components/event/eventSummary.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const EventSummary = () => {
+const EventSummary = ({ event }) => {
   return (
     <div>
       <div className="card z-depth-1 event-summary  red darken-2 hoverable">
         <div className="card-content ">
-          <span className="card-title ">Event Title</span>
+          <span className="card-title ">{event.title}</span>
           <p>Posted by David</p>
           <p className="black-text ">6 August 2021, 9pm</p>
         </div>
