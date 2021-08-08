b8e02cbbb0371276dc257d0212431dfa38c13387
DCarr40 <davidwcarruthers@yahoo.com>
2021-08-08 01:34:14 -0400

    added config folder for firebase and set up firebase and installed firebase

--- src/store/actions/eventActions.js
+++ src/store/actions/eventActions.js
@@ -3,6 +3,7 @@ export const createEvents = (event) => {
     //thunk lets me return a function that temp pause dispatch to do some other stuff first
     return (dispatch, getState) => {
       //asyn call to DB
+      //I can save data or retrieve data from DB here
       dispatch({ type: "CREATE_EVENT", event: event });
     };
   };
