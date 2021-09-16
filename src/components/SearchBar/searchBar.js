7cb9d6fb4847519aba7d62cd7f48c23637cf868d
DCarr40 <davidwcarruthers@yahoo.com>
2021-09-16 11:44:15 -0400

    erased blank lines in searchbarjs

--- src/components/SearchBar/searchBar.js
+++ src/components/SearchBar/searchBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "./searchBar.css";
-
 const SearchBar = () => {
   const [filters, setFilters] = useState("");
 
